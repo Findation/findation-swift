0a9934d46147487594f605f9f602e58ff8b359ab
John Han <work.johnhan@gmail.com>
2025-08-03 16:15:31 +0900

    Initial Commit

---
+++ Findation/FindationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindationApp.swift
+//  Findation
+//
+//  Created by Yoy0z-maps on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FindationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FindationTests/FindationTests.swift
@@ -0,0 +1,17 @@
+//
+//  FindationTests.swift
+//  FindationTests
+//
+//  Created by Yoy0z-maps on 8/3/25.
+//
+
+import Testing
+@testable import Findation
+
+struct FindationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
