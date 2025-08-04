72a434306288ffca554e274b6915fd5e9a152666
John Han <work.johnhan@gmail.com>
2025-08-04 22:23:47 +0900

    FEAT: 탭바 설정

---
+++ Findation/Core/MyPage/View/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  Findation
+//
+//  Created by Yoy0z-maps on 8/4/25.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
