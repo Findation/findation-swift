71e88a76a5815874778dcd178cbdcef2710ccdc0
John Han <work.johnhan@gmail.com>
2025-08-06 17:26:12 +0900

    FEAT: AAPLE_AUTHENTICATION

--- Findation/Constants/ApiURL.swift
+++ Findation/Constants/ApiURL.swift
@@ -13,12 +13,24 @@ enum API {
     enum Auth {
         static let socialLogin = "\(baseURL)/users/auth/social-login/"
         static let tokenRefresh = "\(baseURL)/users/auth/token/refresh/"
+        static let searchUser = "\(baseURL)/users/search/"
     }
     
     enum Routines {
         static let routineList = "\(baseURL)/routines/"
         static func routineDetail(id: String) -> String {
             return "\(baseURL)/routines/\(id)/"
+        }
+    }
+    
+    enum Friends {
+        static let friends = "\(baseURL)/friends/"
+        static let friendsList = "\(baseURL)/friends/list/"
+        static func friendDetail (id: String) -> String {
+            return "\(baseURL)/friends/\(id)/"
+        }
+        static func friendRoutines (id: String) -> String {
+            return "\(baseURL)/friends/routines/\(id)/"
         }
     }
 }
